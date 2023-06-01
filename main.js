430d622a86ad7aa7727fb62c90cd6631c66705d6
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2023-06-01 18:48:54 +0100

    testing

--- main.js
+++ main.js
@@ -1,29 +1,46 @@
-const plus = document.querySelector(".plus"),
- minus = document.querySelector(".minus"),
- num = document.querySelector(".num");
 
-
- let a = 1; 
-
- plus.addEventListener("click", () => {
-
-a++;
-a= ( a < 10) ? "0" + a : a;
-num.innerText = a;
-console.log("a");
-
- });
-
-
- minus.addEventListener("click", () => {
- if( a > 1){
-
-a--;
-a = ( a < 10) ? "0" + a : a;
-num.innerText= a;
-
- }
+  // Obtener elementos relevantes del DOM
+  const minusBtn = document.querySelector('.minus');
+  const plusBtn = document.querySelector('.plus');
+  const numElement = document.querySelector('.num');
+  
+  // Manejar el evento de clic en el botón "-"
+  minusBtn.addEventListener('click', function() {
+    let currentNum = parseInt(numElement.textContent);
+    if (currentNum > 1) {
+      currentNum--;
+      numElement.textContent = currentNum;
+    }
+  });
+  
+  // Manejar el evento de clic en el botón "+"
+  plusBtn.addEventListener('click', function() {
+    let currentNum = parseInt(numElement.textContent);
+    currentNum++;
+    numElement.textContent = currentNum;
+  });
+  
+  // Enviar el formulario por correo electrónico
+  function submitForm() {
+    const name = document.getElementById('name').value;
+    const number = document.getElementById('number').value;
+    const quantity = parseInt(numElement.textContent);
     
-   
+    const subject = 'Nuevo pedido';
+    const nextURL = 'https://pandeyuca.vercel.app/thanks.html';
     
-     });
+    const emailBody = `
+      Name: ${name}
+      Phone number: ${number}
+      Quantity: ${quantity}
+    `;
+    
+    const form = document.getElementById('msform');
+    form.setAttribute('action', 'https://formsubmit.co/moncayomichael23@gmail.com');
+    form.setAttribute('method', 'POST');
+    form.insertAdjacentHTML('beforeend', `<input type="hidden" name="_subject" value="${subject}">`);
+    form.insertAdjacentHTML('beforeend', `<input type="hidden" name="_next" value="${nextURL}">`);
+    form.insertAdjacentHTML('beforeend', `<input type="hidden" name="emailBody" value="${emailBody}">`);
+    
+    form.submit();
+  }
